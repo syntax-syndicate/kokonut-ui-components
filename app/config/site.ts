b63c66f8d3e4148f5b9cbae59b3472ffbd655808
Dorian Baffier <dorianbaffier@gmail.com>
2024-11-08 16:27:05 +0900

    update metadata

--- app/config/site.ts
+++ app/config/site.ts
@@ -3,7 +3,7 @@ export const siteConfig = {
     url: "https://kokonut.dev",
     ogImage: "https://kokonut.dev/og.jpg",
     description:
-        "A collection of modern, interactive, customizable UI components built with Shadcn & Tailwind CSS. Easy to install",
+        "A collection of modern, interactive, customizable UI components built with Shadcn & Tailwind CSS. Ready to make your next project faster and beautiful.",
     links: {
         twitter: "https://twitter.com/dorian_baffier",
         github: "https://github.com/kokonut-labs/kokonutui",
