f8ba932b1656180e6b92e3beec789e0bf88934a5
Dorian Baffier <dorianbaffier@gmail.com>
2024-11-12 20:51:37 +0900

    fix/layout

--- app/docs/layout.tsx
+++ app/docs/layout.tsx
@@ -24,7 +24,7 @@ export default function DocsLayout({
             </p>
             <div className="mx-auto max-w-[1400px] px-4 sm:px-6 lg:px-8">
                 {/* Three-column layout */}
-                <div className="flex md:gap-8 pt-12 md:pt-32">
+                <div className="flex md:gap-8 py-12 md:py-32">
                     {/* Left Navigation */}
                     <div className="md:w-[240px] md:flex-shrink-0">
                         <div className="md:sticky md:top-24">
