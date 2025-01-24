f1db0702b8fb001d4a92a7ab5960a6aa3a196128
Dorian Baffier <dorianbaffier@gmail.com>
2025-01-24 10:24:03 +0900

    fix/analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ import { cn } from "@/lib/utils";
 import { META_THEME_COLORS, siteConfig } from "@/config/site";
 import { RootProvider } from "fumadocs-ui/provider";
 import Head from "next/head";
+import Script from "next/script";
 
 const geistSans = localFont({
     src: "./fonts/GeistVF.woff",
@@ -56,13 +57,6 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en" suppressHydrationWarning>
-            <Head>
-                <script
-                    defer
-                    src="https://cloud.umami.is/script.js"
-                    data-website-id="edae968b-0879-4fd7-a6e8-50409c8e6131"
-                />
-            </Head>
             <body
                 className={cn(
                     geistSans.variable,
@@ -83,6 +77,11 @@ export default function RootLayout({
                     </ThemeProvider>
                 </RootProvider>
                 <Analytics />
+                <Script
+                    defer
+                    src="https://cloud.umami.is/script.js"
+                    data-website-id="edae968b-0879-4fd7-a6e8-50409c8e6131"
+                />
             </body>
         </html>
     );
