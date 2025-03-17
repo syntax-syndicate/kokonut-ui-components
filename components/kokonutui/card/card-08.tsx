83a51b83f1349c1976d2959e7fedcabf4075c451
Dorian Baffier <dorianbaffier@gmail.com>
2025-03-17 16:23:50 +0800

    fix: card-08

--- components/kokonutui/card/card-08.tsx
+++ components/kokonutui/card/card-08.tsx
@@ -40,6 +40,7 @@ export default function Card08({
                         src={image}
                         alt={title}
                         fill
+                        sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
                         className="object-cover"
                     />
                 </div>
