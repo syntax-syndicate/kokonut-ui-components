db3abe2c953abf8b22a08009eff11cf41c8c7f4c
Dorian Baffier <dorianbaffier@gmail.com>
2025-05-13 23:47:25 +0900

    add: oss link

--- app/docs/layout.tsx
+++ app/docs/layout.tsx
@@ -3,6 +3,7 @@ import { DocsLayout } from "fumadocs-ui/layouts/notebook";
 import type { ReactNode } from "react";
 import { baseOptions } from "../layout.config";
 import type { Metadata } from "next";
+import Link from "next/link";
 
 export const metadata: Metadata = {
     title: {
@@ -21,14 +22,18 @@ export default function Layout({ children }: { children: ReactNode }) {
                 defaultOpenLevel: 1,
                 banner: (
                     <div className="border border-zinc-200 dark:border-zinc-800 text-white dark:text-zinc-900 p-2 rounded-md text-xs py-1 flex items-center gap-2">
-                        <div className="text-gray-600 dark:text-gray-400  transition-colors flex items-center gap-1.5 text-xs hover:text-gray-800 dark:hover:text-gray-300 group hover:font-medium hover:cursor-pointer">
+                        <Link
+                            href="https://vercel.com/blog/spring25-oss-program"
+                            target="_blank"
+                            className="text-gray-600 dark:text-gray-400  transition-colors flex items-center gap-1.5 text-xs hover:text-gray-800 dark:hover:text-gray-300 group hover:font-medium hover:cursor-pointer"
+                        >
                             <span className="hidden md:flex items-center gap-2">
                                 <span className="text-transparent bg-gradient-to-r from-orange-500 via-amber-500 to-orange-600 dark:from-orange-400 dark:via-amber-400 dark:to-orange-500 bg-clip-text font-semibold tracking-tighter flex flex-col">
                                     We are now part of the Vercel Open Source
                                     program for Spring 2025!
                                 </span>
                             </span>
-                        </div>
+                        </Link>
                         <svg
                             className="hidden dark:block"
                             viewBox="0 0 256 222"
